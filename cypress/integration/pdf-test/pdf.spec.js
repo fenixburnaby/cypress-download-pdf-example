d15b7a19d47da6a238b67701539e1f0321543aba
Randhal Ramirez <randhalramirez@gmail.com>
2022-04-22 20:35:58 -0600

    adding variable url

--- cypress/integration/pdf-test/pdf.spec.js
+++ cypress/integration/pdf-test/pdf.spec.js
@@ -1,12 +1,15 @@
 describe('compare 2 PDF files', () => {
+
+    const serverName = 'http://localhost:3000';
+
     beforeEach(() => {
-        cy.visit('http://localhost:3000')
+        cy.visit(serverName)
     })
 
     it('Download and compare PDFs succesfully', () => {
 
         cy.get('#capePdf').invoke('attr', 'href').then(linkUrl => {
-            cy.downloadFile('http://localhost:3000'+linkUrl,'data/actualPdfs','cape.pdf');
+            cy.downloadFile(serverName+linkUrl,'data/actualPdfs','cape.pdf');
 
             cy.task("pdfCompare", {
                 actualPdf: "cape.pdf",
@@ -20,7 +23,7 @@ describe('compare 2 PDF files', () => {
     it('Fails when PDFs are different', () => {
 
         cy.get('#newsPdf').invoke('attr', 'href').then(linkUrl => {
-            cy.downloadFile('http://localhost:3000'+linkUrl,'data/actualPdfs','news.pdf');
+            cy.downloadFile(serverName+linkUrl,'data/actualPdfs','news.pdf');
 
             cy.task("pdfCompare", {
                 actualPdf: "news.pdf",
